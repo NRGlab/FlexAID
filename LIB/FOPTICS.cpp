5bc54cbf1b5221f246a7173ac521d4fbbd2005ad
LP <lmorency@me.com>
2015-06-15 23:22:59 -0400

    currently implementing RandomProj::computeSetBounds

--- LIB/FOPTICS.cpp
+++ LIB/FOPTICS.cpp
@@ -6,42 +6,128 @@
 // Constructor
 FastOPTICS::FastOPTICS(FA_Global* FA, GB_Global* GB, VC_Global* VC, chromosome* chrom, genlim* gene_lim, atom* atoms, resid* residue, gridpoint* cleftgrid, int num_chrom)
 {
-	this->FastOPTICS::Initialize(FA,GB,VC,chrom,gene_lim,atoms,residue,cleftgrid,nChrom);
-
-	RandomProjectedNeighborsAndDensities::RandomProjectedNeighborsAndDensities algo(, minPts);
-
+	FastOPTICS::Initialize(FA,GB,VC,chrom,gene_lim,atoms,residue,cleftgrid,num_chrom);
+	
+	// vector of point indexes 
+	std::vector< int > ptInd;
+	for(int k = 0; k < this->N; ++k) ptInd.push_back(k);
+	
+	RandomProjectedNeighborsAndDensities::RandomProjectedNeighborsAndDensities MultiPartition(this->points, this->minPts);
+	MultiPartition.computeSetBounds(ptInd);
 
 }
 
-FastOPTICS::Initialize(FA_Global* FA, GB_Global* GB, VC_Global* VC, chromosome* chrom, genlim* gene_lim, atom* atoms, resid* residue, gridpoint* cleftgrid, int num_chrom)
+FastOPTICS::Initialize(FA_Global* FA, GB_Global* GB, VC_Global* VC, chromosome* chroms, genlim* gene_lim, atom* atoms, resid* residue, gridpoint* cleftgrid, int num_chrom)
 {
 	// FlexAID
-	this->N = nChrom;
-	this->FA = FA;
-	this->GB = GB;
-	this->VC = VC;
+	int this->N = nChrom;
+	FA_Global* FastOPTICS::FA = FA;
+	GB_Global* FastOPTICS::GB = GB;
+	VC_Global* FastOPTICS::VC = VC;
+	chromosome* FastOPTICS::chroms = chroms;
+	int FastOPTICS::minPts = (int)(0.01*this->N - 0.5);
+	int FastOPTICS::nDimensions = this->FA->npar + 2; // 3 Dim for first gene (translational) + 1 Dim per gene = nGenes + 2
+	gene_lim* FastOPTICS::gene_lim = gene_lim;
+	atom* FastOPTICS::atoms = atoms;
+	resid* FastOPTICS::residue = residue;
+	gridpoint* FastOPTICS::cleftgrid = cleftgrid;
 	// initialization from chromosome* chrom (into a pointer or into vector<>)
-	this->gene_lim = gene_lim;
-	this->atoms = atoms;
-	this->residue = residue;
-	this->cleftgrid = cleftgrid;
 
 	// FastOPTICS
-	this->iOrder = 0;
+	int FastOPTICS::iOrder = 0;
+	std::vector< int > 		this->FastOPTICS::order(this->N, 0);
+	std::vector< float > 	this->FastOPTICS::reachDist(this->N, UNDEFINED_DIST);
+	std::vector< bool > 	this->FastOPTICS::processed(this->N, false);
+	std::vector< float > 	this->FastOPTICS::inverseDensities(this->N, 0.0f);
+	this->order.reserve(this->N);
+	this->reachDist.reserve(this->N);
+	this->processed.reserve(this->N);
+	this->inverseDensities.reserve(this->N);
 
-	std::vector< int > FastOPTICS::order(this->N);
-	std::vector< double > FastOPTICS::reachDist(this->N);
-	std::vector< bool > FastOPTICS::processed(this->N);
-	std::vector< double > FastOPTICS::inverseDensities(this->N);
-	std::vector< chromosome* > FastOPTICS::chroms(this->N);
-	std::vector< std::vector< chromosome* > > FastOPTICS::neighs(this->N);
+	std::vector< std::pair< chromosome*,std::vector<float> > > this->FastOPTICS::points;
+	this->points.reserve(this->N);
+	std::vector< std::vector< chromosome* > > this->FastOPTICS::neighs(this->N);
+	this->neighs.reserve(this->N);
 	
 	for(int i = 0; i < this->N; ++i)
 	{
-		this->order.push_back(0);
-		this->reachDist.push_back(UNDEFINED_DIST);
-		this->processed.push_back(false);
-		this->inverseDensities.push_back(0.0);
-		this->chroms.push_back((chromosome*)&chrom[i]);
+		// need to transform the chromosomes into vector f
+		std::vector<float> vChrom(FastOPTICS::vectorize_chrom(&chroms[i])); // Copy constructor
+		if(vChrom.size() == this->nDimensions)
+			// std::pair<first, second> is pushed to this->points[]
+			// 	first  -> chromosome* pChrom (pointer to chromosome)
+			// 	second -> vChrom[FA->npar+n] (vectorized chromosome)
+			this->points.push_back( std::make_pair( (chromosome*)&chroms[i], vChrom) ) ;
+	}
+}
+
+std::vector<float> FastOPTICS::vectorize_chromosome(chromsome* chrom)
+{
+	std::vector<float> vChrom(this->nDimensions, 0.0f);
+	// getting 
+	for(int j = 0; j < this->nDimensions, ++j)
+	{
+		if(j == 0) //  building the first 3 comp. from genes[0] which are CartCoord x,y,z
+			for(int i = 0; i < 3; ++i)
+				vChrom[i] = (float) this->cleftgrid[(unsigned int)(*chrom).genes[j].to_ic].coor[i] - this->FA->ori[i];
+		else
+		{
+			// j+2 is used from {j = 1 to N} to build further comp. of genes[j]
+			vChrom[j+2] = (float) genetoic(&gene_lim[j], (*chrom).genes[j].to_int32);
+		}
+	}
+	return vChrom;
+}
+
+/*****************************************\
+			RandomProjections
+\*****************************************/
+// Constructor
+RandomProjectedNeighborsAndDensities::RandomProjectedNeighborsAndDensities(std::vector< std::pair< chromosome*,std::vector<float> > >& points, int minSplitSize)
+{
+	this->minSplitSize = minSplitSize;
+
+	if( !points || points.empty() )
+	{
+		this->N = 0;
+		this->nDimensions = 0;
+		this->nProject1D = 0;
+		return;
+	}
+	else
+	{
+		this->N = this->points.size();
+		this->nDimensions = (this->points[0].second).size();
+		this->nPointsSetSplits = (int) this->logOProjectionConstant * log(this->N * this->nDimensions + 1)/log(2);
+		this->nProject1D = (int) this->logOProjectionConstant * log(this->N * this->nDimensions + 1)/log(2);
+		// line below calls copy-constructor
+		std::vector< std::pair< chromosome*,std::vector<float> > > this->RandomProjectedNeighborsAndDensities::points(points);
+		// construct splitsets && projectPoints by calling vector constructor
+		// default constructor for empty vector for splitsets
+		std::vector< std::vector< int > > this->RandomProjectedNeighborsAndDensities::splitsets;
+		// default constructor for size this->nProject1D for projectedPoints[nProject1D][N]
+		std::vector< std::vector<float> > this->RandomProjectedNeighborsAndDensities::projectedPoints;
+		// std::vector< std::vector<float> > RandomProjectedNeighborsAndDensities::projectedPoints(this->nProject1D);
+	}
+}
+
+void RandomProjectedNeighborsAndDensities::computeSetBounds(std::vector< int > & ptList)
+{
+	std::vector< std::vector<float> > tempProj;
+	// projecting points
+	for(int j = 0; j<this->nProject1D; ++j)
+	{
+		std::vector<float> currentRp(this->nDimensions,0.0f);
+		for(int i=0; i<this->nDimensions; ++i)
+		{
+			if(i==0) // First 3Dim are defined by the grid on genes[0]
+			{
+
+			}
+			else // other Dim are defined by subsequent genes[i] ? (i > 0)
+			{
+
+			}
+		}
 	}
 }
--- LIB/FOPTICS.h
+++ LIB/FOPTICS.h
@@ -3,66 +3,76 @@
 
 #include "gaboom.h"
 #include "boinc.h"
+#include <utility>
 
 #define UNDEFINED_DIST -0.1f // Defined in FOPTICS as > than +INF
-#define minPts 5
-/*****************************************\
-			RandomProjections
-\*****************************************/
-class RandomProjectedNeighborsAndDensities
-{
-	public:
-		RandomProjectedNeighborsAndDensities(); // Constructor (publicly called from FlexAID *_cluster.cxx)
-		void computeSetBounds();
-	
-	private:
-		int N;
-		int nDimensions;
-		static const int RandomSeed;
-		static const int logOProjectionConstant = 20;
-		static const double sizeTolerance = (double) 2.0/3.0;
-		std::vector< chromosome* > chroms;
-		int minSplitSize;
-
-	protected:
-		int nProject1D;
-		std::vector< std::vector< int > > splitsets;
-		std::vector< std::vector< double > > projectedPoints;
-};
 
 /*****************************************\
 				FastOPTICS
 \*****************************************/
 class FastOPTICS
 {
-	friend class RandomProjectedNeighborsAndDensities;
-	
+	// friend class RandomProjectedNeighborsAndDensities;
 	public:
 		FastOPTICS(FA_Global* FA, GB_Global* GB, VC_Global* VC, chromosome* chrom, genlim* gen_lim, atom* atoms, resid* residue, gridpoint* cleftgrid, int nChrom); // Constructor (publicly called from FlexAID's *_cluster.cxx)
 	
 	private:
 		// FlexAID specific attributes
-		int N;	// N : number of chromosomes to cluster
-		FA_Global* FA;			// pointer to FA_Global struct
-		GB_Gblobal* GB;			// pointer to GB_Global struct
-		VC_Global* VC;			// pointer to VC_Global struct
-		chromosome* chrom;		// pointer to chromosomes' array
-		genlim* gene_lim;		// pointer to gene_lim genlim array (useful for bondaries defined for each gene)
-		atom* atoms;			// pointer to atoms' array
-		resid* residue;			// pointer to residues' array
-		gridpoint* cleftgrid;	// pointer to gridpoints' array (defining the total search space of the simulation)
+		int N;					// N : number of chromosomes to cluster
+		static int nDimensions;
 		
 		// FOPTICS algorithm attributes
 		static int iOrder;
+		static int minPoint;	// minPts : minimal number of neighbors (only parameter in FOPTICS)
 		std::vector< int > order;
-		std::vector< double > reachDist;
+		std::vector< float > reachDist;
 		std::vector< bool > processed;
-		std::vector< double > inverseDensities;
-		std::vector< chromosome* > points;
+		std::vector< float > inverseDensities;
+		std::vector< std::pair<chromosome*,std::vector<float>> > points;
 		std::vector< std::vector< chromosome* > > neighs;
 		
 		// private methods
 		void Initialize(FA_Global* FA, GB_Global* GB, VC_Global* VC, chromosome* chrom, genlim* gen_lim, atom* atoms, resid* residue, gridpoint* cleftgrid, int nChrom); // Initialize FastOPTICS private attributes from FlexAID structs
+	
+	protected:
+		// protected methods to be used by RandomProjectedNeighborsAndDensities::methods()
+		static FA_Global* FA;			// pointer to FA_Global struct
+		static GB_Gblobal* GB;			// pointer to GB_Global struct
+		static VC_Global* VC;			// pointer to VC_Global struct
+		static chromosome* chroms;		// pointer to chromosomes' array
+		static genlim* gene_lim;		// pointer to gene_lim genlim array (useful for bondaries defined for each gene)
+		static atom* atoms;				// pointer to atoms' array
+		static resid* residue;			// pointer to residues' array
+		static gridpoint* cleftgrid;	// pointer to gridpoints' array (defining the total search space of the simulation)
+		std::vector<float> vectorize_chromosome(chromsome* chrom);
+};
+
+/*****************************************\
+			RandomProjections
+\*****************************************/
+class RandomProjectedNeighborsAndDensities
+{
+	friend class FastOPTICS;
+	
+	public:
+		RandomProjectedNeighborsAndDensities(std::vector< std::pair< chromosome*,std::vector<float> > >&, int); // Constructor (publicly called from FlexAID *_cluster.cxx)
+	
+	private:
+		int N;
+		int nDimensions;
+		int minSplitSize;
+		static const int logOProjectionConstant = 20;
+		static const float sizeTolerance = (float) 2.0f/3.0f;
+		std::vector< std::pair<chromosome*,std::vector<float>> > points;
+
+	protected:
+		// protected attributes (accessible via FastOPTICS class)
+		int nProject1D;				// CONSTANT c0
+		int nPointsSetSplits;		// CONSTANT c1
+		std::vector< std::vector< int > > splitsets;
+		std::vector< std::vector< float > > projectedPoints;
+		// protected methods (accessible via FastOPTICS class)
+		void computeSetBounds(std::vector< int >&);
 };
 
 #endif
