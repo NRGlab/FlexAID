f54913a86571f38019f1b872bcf75d7011b1c65c
LP <lmorency@me.com>
2018-02-26 17:57:37 -0500

    quick fix for function calls in solvated_complex_energy()

--- LIB/BindingMode.cpp
+++ LIB/BindingMode.cpp
@@ -568,7 +568,7 @@ double BindingMode::compute_solvated_complex_entropy() const
 double BindingMode::compute_solvated_complex_energy() const
 {
 	// double energy = 0.0;
-    double energy = ( this->compute_solvated_complex_enthalpy() - ( this->Population->Temperature * this->compute_complex_entropy()) );
+    double energy = ( this->compute_solvated_complex_enthalpy() - ( this->Population->Temperature * this->compute_solvated_complex_entropy()) );
 
 	// compute
 	
